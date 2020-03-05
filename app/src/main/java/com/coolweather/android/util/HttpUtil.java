675fdd82d16ab3d326b7ece2802da0197fcc4ff8
Jia Jia <jiafujia123@gmail.com>
2020-03-05 16:31:55 +0900

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.util;
+
+import android.util.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        Log.d("HttpUtil", address);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
+
